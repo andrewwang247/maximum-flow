c493e01f2f6144722fbbb9ad5fb526140cb7c80b
Andrew Wang <siweiw9@gmail.com>
2026-09-01 13:14:39 -0700

    convert to pyproject and ruff

---
+++ main.py
@@ -0,0 +1,47 @@
+"""Compute maximum flow on flow network.
+
+Copyright 2026. Andrew Wang.
+"""
+
+import logging
+from pathlib import Path
+
+import numpy as np
+from click import Path as cPath
+from click import command, option
+
+from src import create_network
+
+# pylint: disable=no-value-for-parameter
+
+
+@command()
+@option(
+    "--filename",
+    "-f",
+    required=True,
+    type=cPath(exists=True, file_okay=True, dir_okay=False, readable=True),
+    help="Path to flow network specification.",
+)
+@option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    default=False,
+    help="Set verbosity of solving process.",
+)
+def main(filename: str, *, verbose: bool) -> None:
+    """Compute maximum flow on flow network."""
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.WARNING)
+    network = create_network(Path(filename))
+    max_flow, flow_matrix = network.maximum_flow()
+    print(f"Maximum flow = {max_flow}")
+    dim_1, dim_2 = np.nonzero(network.capacity)
+    for src, dst in zip(dim_1, dim_2, strict=True):
+        flow = flow_matrix[src, dst]
+        capacity = network.capacity[src, dst]
+        print(f"{src} -> {dst} : {flow} / {capacity}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_flow.py
@@ -0,0 +1,45 @@
+"""Pytest to run unit tests.
+
+Copyright 2026. Andrew Wang.
+"""
+
+from json import load
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from src import create_network
+
+_TEST_DIR = "test"
+
+
+def _get_tests() -> list[tuple[Path, int]]:
+    """Get parametrized tests for pytest."""
+    solutions = Path(_TEST_DIR) / "answers.json"
+    with solutions.open(encoding="UTF-8") as fp:
+        answers: dict[str, int] = load(fp)
+    tests: list[tuple[Path, int]] = []
+    for network, solution in answers.items():
+        fpath = Path(_TEST_DIR) / f"{network}.txt"
+        assert fpath.is_file(), f"Test file {fpath} does not exist"
+        tests.append((fpath, solution))
+    return tests
+
+
+@pytest.mark.parametrize(("filepath", "expected_max"), _get_tests())
+def test_flow(filepath: Path, expected_max: int) -> None:
+    """Test network flow on filename."""
+    network = create_network(filepath)
+    max_flow, flow = network.maximum_flow()
+    assert max_flow == expected_max, "Max flow is incorrect."
+    assert np.array_equal(np.transpose(flow), -flow), "Flow is not skew symmetric"
+    assert (flow <= network.capacity).all(), "Flow exceeds capacity."
+    assert np.sum(flow[network.source, :]) == expected_max, (
+        "Input from source is incorrect."
+    )
+    assert np.sum(flow[:, network.sink]) == expected_max, "Output to sink is incorrect."
+    for idx, net_flow in enumerate(np.sum(flow, axis=1)):
+        assert net_flow == 0 or idx in (network.source, network.sink), (
+            f"Flow conservation is violated at vertex {idx}."
+        )
