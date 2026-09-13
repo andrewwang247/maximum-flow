2305ae70718082b90bb53a61abda6fadcb6d8e10
Andrew Wang <siweiw9@gmail.com>
2026-09-13 15:31:00 -0700

    make tests more readable

--- test/test_flow.py
+++ test/test_flow.py
@@ -9,37 +9,40 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from src import create_network
+from src import FlowNetwork, create_network
 
-_TEST_DIR = "test"
+_TEST_DIR = Path("test")
 
 
-def _get_tests() -> list[tuple[Path, int]]:
+def _get_tests() -> list[tuple[FlowNetwork, int]]:
     """Get parametrized tests for pytest."""
-    solutions = Path(_TEST_DIR) / "answers.json"
+    solutions = _TEST_DIR / "answers.json"
     with solutions.open(encoding="UTF-8") as fp:
         answers: dict[str, int] = load(fp)
-    tests: list[tuple[Path, int]] = []
-    for network, solution in answers.items():
-        fpath = Path(_TEST_DIR) / f"{network}.txt"
-        assert fpath.is_file(), f"Test file {fpath} does not exist"
-        tests.append((fpath, solution))
-    return tests
+    params: list[tuple[FlowNetwork, int]] = []
+    for nw, expected_max in answers.items():
+        filepath = _TEST_DIR / f"{nw}.txt"
+        assert filepath.is_file()
+        assert expected_max > 0
+        network = create_network(filepath.open(encoding="UTF-8"))
+        params.append((network, expected_max))
+    return params
 
 
-@pytest.mark.parametrize(("filepath", "expected_max"), _get_tests())
-def test_flow(filepath: Path, expected_max: int) -> None:
+@pytest.mark.parametrize(("network", "expected_max"), _get_tests())
+def test_flow(network: FlowNetwork, expected_max: int) -> None:
     """Test network flow on filename."""
-    network = create_network(filepath.open(encoding="UTF-8"))
     max_flow, flow = network.maximum_flow()
-    assert max_flow == expected_max, "Max flow is incorrect."
-    assert np.array_equal(np.transpose(flow), -flow), "Flow is not skew symmetric"
+
+    assert max_flow == expected_max
+    assert np.array_equal(flow.T, -flow), "Flow is not skew symmetric"
     assert (flow <= network.capacity).all(), "Flow exceeds capacity."
-    assert np.sum(flow[network.source, :]) == expected_max, (
-        "Input from source is incorrect."
-    )
-    assert np.sum(flow[:, network.sink]) == expected_max, "Output to sink is incorrect."
+
+    from_source = np.sum(flow[network.source, :])
+    to_sink = np.sum(flow[:, network.sink])
+    assert from_source == expected_max == to_sink
+
     for idx, net_flow in enumerate(np.sum(flow, axis=1)):
-        assert net_flow == 0 or idx in (network.source, network.sink), (
-            f"Flow conservation is violated at vertex {idx}."
-        )
+        if idx in (network.source, network.sink):
+            continue
+        assert net_flow == 0, f"Flow conservation violated at vertex {idx}."
